22bb70402b3383fad3d0b03e83381c31940d5887
Doniyor Asadullaev <asadullaevdonier@gmail.com>
2022-12-15 22:35:14 +0500

    composer troubles

--- index.php
+++ index.php
@@ -4,5 +4,4 @@ require_once __DIR__.'/vendor/autoload.php';
 use Class\Logger\Logger;
 
 $log=new Logger();
-echo $log->show();
-//$log->write(['level'=>'Fatal Error','message'=>'Parse Error','context'=>'There is a problem parsing the package']);
+$log->write(['level'=>'Fatal Error','message'=>'Parse Error','context'=>'There is a problem parsing the package']);
--- src/Class/Logger/Logger.php
+++ src/Class/Logger/Logger.php
@@ -2,18 +2,18 @@
 
 namespace Class\Logger;
 
-//use Interface\LoggerInterface;
+use Interface\LoggerInterface\LoggerInterface;
 
-class Logger //implements LoggerInterface
+class Logger implements LoggerInterface
 {
-    /*public function write(array $context):void
+    public function write(array $context):void
     {
         extract($context);
 
         $date = date('d.m.Y H:i:s');
         $log = "Дата : $date | Уровень : $level | Сообщение : $message | Контекст : $context ";
 
-        $file=fopen("logs.txt", 'w');
+        $file=fopen("./public/logs.txt", 'w');
 
         $file.="\r\n".$log;
     }
@@ -24,8 +24,5 @@ class Logger //implements LoggerInterface
     public function log(array $context): void
     {
         // TODO: Implement log() method.
-    }*/
-    public function show(){
-        return __CLASS__.':'.__FILE__;
     }
 }
